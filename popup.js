aea5eb77e68e3540f659d0c5043b3d45fd3248e9
U-TMS\170926 <170926@D9705349.tms.toyota.com>
2013-09-18 17:40:29 -0700

    Updated manifest.json so the icon shows up in the browser bar. Updated popup.js so that blank lines are skipped.

--- popup.js
+++ popup.js
@@ -1,12 +1,23 @@
-function myAlert(){
-    var lines = $('textarea').val().split('\n');
+function open_links_in_tabs(){
 
-    for ( var i = 0; i < lines.length; i++ ){
+    // links is an array containing the contents of the textarea in the form
+    // each array element is one line from the form terminated by a newline
+    var links = $('textarea').val().split('\n');
+
+    // process each link
+    for ( var i = 0; i < links.length; i++ ){
+        
         // add code to skip blank lines
-        chrome.tabs.create({url:lines[i]});
+        if (links[i].length < 1 ) continue;
+
+        // add code to check that a proper URL is formed
+
+        
+        // and finally, open the URL in tabs
+        chrome.tabs.create({url:links[i]});
     }
 }
 
 document.addEventListener('DOMContentLoaded', function () {
-    document.getElementById('go_button').addEventListener('click', myAlert);
+    document.getElementById('go_button').addEventListener('click', open_links_in_tabs);
 });
